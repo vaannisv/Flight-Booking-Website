d3602a58cbe5a014dc964b9255c60b559e1c0b75
Vanni <vaanichaudhary666@gmail.com>
2024-09-11 19:36:49 +0530

    started

---
+++ 02Flight/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import "./NavbarStyles.css" 
+
+import React, { Component } from 'react'
+
+class Navbar extends Component{
+    render(){
+        <div className="navbarItems">
+            <h1 className="navbarLogo">AirJet</h1>
+            
+            <ul className="navMenu">
+                <li><Link>Home</Link></li>
+                <li><Link>Home</Link></li>
+                <li><Link>Home</Link></li>
+                <li><Link>Home</Link></li>
+            </ul>
+        </div>
+    }
+}
+
+export default Navbar;
+
+
